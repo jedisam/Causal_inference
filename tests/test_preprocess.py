71fcad4887a1d2a848e657d4e37e913d72114bba
Yididiya <yidisam18@gmail.com>
2022-07-03 16:11:15 +0300

    naming error fix

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -10,7 +10,7 @@ sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../scripts"))
 )
 
-from preprocessing import Preprocess
+from preprocessing import PreProcess
 
 
 class TESTPHARMASALES(unittest.TestCase):
